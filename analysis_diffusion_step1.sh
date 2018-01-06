80fda7276958384c4d43bf2bdc2d4d0cb5709e6a
Erin Mazerolle <erinmaz@gmail.com>
2018-01-06 12:10:49 -0700

    changed analysis_diffusion_step1.sh to work on only one directory

--- analysis_diffusion_step1.sh
+++ analysis_diffusion_step1.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 #subject IDs as input
-MYSUB_PRE=$1
-MYSUB_POST=$2
+MYSUB=$1
 ANALYSISDIR=/home/erinmazerolle/MRGFUS/analysis
 SCRIPTSDIR=/home/erinmazerolle/MRGFUS/scripts
-DIFFDIR_PRE=${ANALYSISDIR}/${MYSUB_PRE}/diffusion
-DIFFDIR_POST=${ANALYSISDIR}/${MYSUB_POST}/diffusion
-cp ${SCRIPTSDIR}/bvecs ${DIFFDIR_PRE}/.
-cp ${SCRIPTSDIR}/bvecs ${DIFFDIR_POST}/.
-cp ${SCRIPTSDIR}/bvals ${DIFFDIR_PRE}/.
-cp ${SCRIPTSDIR}/bvals ${DIFFDIR_POST}/.
-bedpostx ${DIFFDIR_PRE} &
-bedpostx ${DIFFDIR_POST}
-
+DIFFDIR=${ANALYSISDIR}/${MYSUB}/diffusion
+cp ${SCRIPTSDIR}/bvecs ${DIFFDIR}/.
+cp ${SCRIPTSDIR}/bvals ${DIFFDIR}/.
+bedpostx ${DIFFDIR} 
