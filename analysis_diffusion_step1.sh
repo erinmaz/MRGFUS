eace8156db88c7797a30ca0c3943de928eac26c8
Erin Mazerolle <erinmaz@gmail.com>
2018-01-06 12:13:29 -0700

    fixed directories in analysis_diffusion_step1.sh

--- analysis_diffusion_step1.sh
+++ analysis_diffusion_step1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #subject IDs as input
 MYSUB=$1
-ANALYSISDIR=/home/erinmazerolle/MRGFUS/analysis
-SCRIPTSDIR=/home/erinmazerolle/MRGFUS/scripts
+MAINDIR=/Users/erin/Desktop/Projects/MRGFUS
+SCRIPTSDIR=${MAINDIR}/scripts
+ANALYSISDIR=${MAINDIR}/analysis
 DIFFDIR=${ANALYSISDIR}/${MYSUB}/diffusion
 cp ${SCRIPTSDIR}/bvecs ${DIFFDIR}/.
 cp ${SCRIPTSDIR}/bvals ${DIFFDIR}/.
