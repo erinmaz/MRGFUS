7b31675fad2e093fb59c53165c55df0903245529
Erin Mazerolle <erinmaz@gmail.com>
2018-01-24 14:20:43 -0700

    in seedfc analysis, create backup of working feat dir, view all masks in fsleyes before continuing

--- analysis_step2_seedfc.sh
+++ analysis_step2_seedfc.sh
@@ -3,19 +3,22 @@
 
 MYSUB=$1
 
+MYCOIL=$2 
 #full path of mask (in standard space)
-MYMASK=`imglob $2`
+MYMASK=`imglob $3`
 
 #full path of pre-processed feat (rs.feat in my current pipeline)
-MYFEAT=$3
+MYFEAT=$4
 
 #full path of reg folder to use (rs_reg.feat/reg in my current pipeline)
-MYREG=$4
+MYREG=$5
 
 ANALYSISDIR=~/Desktop/Projects/MRGFUS/analysis
 SCRIPTSDIR=~/Desktop/Projects/MRGFUS/scripts
 MASKNAME=`basename $MYMASK`
 
+FEATNAME=`basename $MYFEAT`
+cp -r $MYFEAT ${ANALYSISDIR}/${MYSUB}/fmri/${FEATNAME}.backup
 
 #get lateral ventricles in func space for nuisance regressor
 invwarp -w ${MYREG}/example_func2standard_warp -o ${MYREG}/standard2example_func_warp -r ${MYREG}/example_func
@@ -31,16 +34,16 @@ fslmaths ${ANALYSISDIR}/${MYSUB}/anat/c1T1 -thr .95 -bin ${ANALYSISDIR}/${MYSUB}
 fslmaths ${MYFEAT}/${MASKNAME}2highres -mas ${ANALYSISDIR}/$MYSUB/anat/c1T1_thr95 ${MYFEAT}/${MASKNAME}2highres_gm
 flirt -applyxfm -init ${MYREG}/highres2example_func.mat -in ${MYFEAT}/${MASKNAME}2highres_gm -ref ${MYREG}/example_func -out ${MYFEAT}/${MASKNAME}2highres_gm2example_func -interp nearestneighbour
 
-fsleyes ${MYREG}/example_func ${MYFEAT}/${MASKNAME}2highres_gm2example_func
+fsleyes ${MYREG}/example_func ${MYFEAT}/${MASKNAME}2highres_gm2example_func ${MYFEAT}/harvardoxford-subcortical_prob_Lateral_Ventricles2func ${MYFEAT}/filtered_func_data -m ${MYFEAT}/mask
 
 
 fslmeants -i ${MYFEAT}/filtered_func_data -m ${MYFEAT}/${MASKNAME}2highres_gm2example_func -o ${MYFEAT}/${MASKNAME}2highres_gm2example_func_ts.txt --eig
 fslmeants -i ${MYFEAT}/filtered_func_data -m ${MYFEAT}/mask -o ${MYFEAT}/global_ts.txt --eig
 fslmeants -i ${MYFEAT}/filtered_func_data -m ${MYFEAT}/harvardoxford-subcortical_prob_Lateral_Ventricles2func -o ${MYFEAT}/lateral_ventricles_ts.txt --eig
 
-sed 's:MYSUB:'${MYSUB}':g' ${SCRIPTSDIR}/fc.fsf > ${ANALYSISDIR}/${MYSUB}/fmri/fc_${MASKNAME}.fsf
+sed 's:MYSUB:'${MYSUB}':g' ${SCRIPTSDIR}/fc_${MYCOIL}.fsf > ${ANALYSISDIR}/${MYSUB}/fmri/fc_${MASKNAME}.fsf
 sed -i '' 's:MYFEAT:'${MYFEAT}':g'  ${ANALYSISDIR}/${MYSUB}/fmri/fc_${MASKNAME}.fsf
-sed -i '' 's:MASKNAME:'${MASKNAME}'2highres_gm2example_func_ts.txt:g' ${ANALYSISDIR}/${MYSUB}/fmri/fc_${MASKNAME}.fsf
+sed -i '' 's:MASKNAME:'${MASKNAME}':g' ${ANALYSISDIR}/${MYSUB}/fmri/fc_${MASKNAME}.fsf
 
 feat ${ANALYSISDIR}/${MYSUB}/fmri/fc_${MASKNAME}.fsf
 
