cb1cea8dfa0f79f8e8e1762379ee774a209e776e
Erin Mazerolle <erinmaz@gmail.com>
2018-01-24 10:14:03 -0700

    change analysis_diffusion_step1.sh to save script to analysis dir

--- analysis_diffusion_step1.sh
+++ analysis_diffusion_step1.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 #subject IDs as input
+
+
+
 MYSUB=$1
 MAINDIR=/Users/erin/Desktop/Projects/MRGFUS
 SCRIPTSDIR=${MAINDIR}/scripts
 ANALYSISDIR=${MAINDIR}/analysis
 DIFFDIR=${ANALYSISDIR}/${MYSUB}/diffusion
+
+#################### SAVE SCRIPT ###################################
+#save a copy of this script to the analysis dir, so I know what I've run
+cp $0 ${ANALYSISDIR}/${MYSUB}/.
+
 cp ${DIFFDIR}/data.eddy_rotated_bvecs ${DIFFDIR}/bvecs
 cp ${SCRIPTSDIR}/bvals ${DIFFDIR}/.
 bedpostx ${DIFFDIR} 
