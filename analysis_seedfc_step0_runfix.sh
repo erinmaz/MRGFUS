968d179f0926a6a60b213a985be84bf319fe95e7
Erin Mazerolle <erinmaz@gmail.com>
2018-01-30 13:58:15 -0700

    actually adding scripts to run FIX ICA artifact cleanup

---
+++ analysis_seedfc_step0_runfix.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ANALYSISDIR=~/Desktop/Projects/MRGFUS/analysis
+
+MYFEAT=$1
+
+cp -r $MYFEAT ${MYFEAT}.backup
+
+cd /Users/erin/fix1.065
+
+./fix ${MYFEAT} ~/fix1.065/training_files/Standard.RData 20
+
+fsleyes ${MYFEAT}/filtered_func_data.ica/melodic_IC
+fsleyes ${MYFEAT}/filtered_func_data_clean
