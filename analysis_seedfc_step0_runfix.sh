fe10103ddf918b4f43a6bfa3e2f7203648d0c228
Erin Mazerolle <erinmaz@gmail.com>
2018-01-30 15:21:08 -0700

    fix script to call fix

--- analysis_seedfc_step0_runfix.sh
+++ analysis_seedfc_step0_runfix.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 MYFEAT=$1
-
+MYREG=$2
 cp -r $MYFEAT ${MYFEAT}.backup
-
+cp -r $MYREG ${MYFEAT}/reg
 cd /Users/erin/fix1.065
+
 
 ./fix ${MYFEAT} ~/fix1.065/training_files/Standard.RData 20
 
 fsleyes ${MYFEAT}/filtered_func_data.ica/melodic_IC
 fsleyes ${MYFEAT}/filtered_func_data_clean
+bbedit ${MYFEAT}/fix4melview_Standard_thr20.txt
