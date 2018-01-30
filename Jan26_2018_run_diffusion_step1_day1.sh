2143f65aea7129f638dbecba020c7bf0426765aa
Erin Mazerolle <erinmaz@gmail.com>
2018-01-30 13:55:22 -0700

    script to run FIX ICA artifact cleanup

--- Jan26_2018_run_diffusion_step1_day1.sh
+++ Jan26_2018_run_diffusion_step1_day1.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-for f in 9001_SH-11692 9002_RA-11833 9004_EP-12203 9006_EO-12487 9007_RB-1291 9009_CRB-13043
+for f in 9001_SH-11692 9002_RA-11833 9004_EP-12203 
+do
+time analysis_diffusion_step1.sh $f &>/Users/erin/Desktop/Projects/MRGFUS/logs/analysis_diffusion_step1_${f}.log &
+done
+
+for f in 9006_EO-12487 9007_RB-12910 9009_CRB-13043
 do
 time analysis_diffusion_step1.sh $f &>/Users/erin/Desktop/Projects/MRGFUS/logs/analysis_diffusion_step1_${f}.log &
 done
