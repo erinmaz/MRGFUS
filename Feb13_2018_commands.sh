cf1bf408d56988c13584a317d1e725102db6480e
Erin Mazerolle <erinmaz@gmail.com>
2018-02-13 20:34:56 -0700

    pipeline for analyzing skeleton within a tractography-generated ROI completed

--- Feb13_2018_commands.sh
+++ Feb13_2018_commands.sh
@@ -1,13 +1,12 @@
 epi_reg --epi=/Users/erin/Desktop/Projects/MRGFUS/analysis/9006_EO-13017/diffusion/dti_noPURE_unwarped_nodif --t1=/Users/erin/Desktop/Projects/MRGFUS/analysis/9006_EO-13017/anat/T1 --t1brain=/Users/erin/Desktop/Projects/MRGFUS/analysis/9006_EO-13017/anat/T1_brain --out=diff_2_T1_bbr
 
 
-for f in 9001_SH-11644 9003_RB-12013 9005_BG-13004 9007_RB-12910 9011_BB-13042 9001_SH-11692 9003_RB-12064 9005_BG-13126 9008_JO-12613 9001_SH-12271 9003_RB-12669 9006_EO-12389 9008_JO-12667 9002_RA-11764 9004_EP-12126 9006_EO-12487 9009_CRB-12609 9002_RA-11833 9004_EP-12203	9009_CRB-13043 9002_RA-12388 9004_EP-12955 9007_RB-12461 9010_RR-13130
+for f in 9001_SH-11644 9003_RB-12013 9005_BG-13004 9007_RB-12910 9011_BB-13042 9001_SH-11692 9003_RB-12064 9005_BG-13126 9008_JO-12613 9001_SH-12271 9003_RB-12669 9006_EO-12389 9008_JO-12667 9002_RA-11764 9004_EP-12126 9006_EO-12487 9009_CRB-12609 9002_RA-11833 9004_EP-12203 9009_CRB-13043 9002_RA-12388 9004_EP-12955 9007_RB-12461 9010_RR-13130
 do
 analysis_diffusion_bbr_reg.sh $f
 done
 
-
-
+9004_EP-12203 9009_CRB-13043 #CHECK THAT THESE RAN
 
 analysis_longitudinal_step1.sh 9001_SH 11644 11692 
 analysis_longitudinal_step1.sh 9002_RA 11764 11833 
@@ -25,3 +24,14 @@ analysis_longitudinal_step2.sh 9004_EP 12203 12955
 analysis_longitudinal_step2.sh 9006_EO 12487 13017
 
 #redo 9003 without extra brain extracted
+
+analysis_longitudinal_step1.sh 9003_RB 12013 12064 
+analysis_diffusion_bbr_reg.sh 9003_RB-12013
+
+cd /Users/erin/Desktop/Projects/MRGFUS/analysis
+for f in `ls -d *diffusion_longitudinal`
+do
+mv ${f}/day1_T1_lesion ${f}/day1_T1_lesion_old_reg
+done
+
+
--- Feb8_2018_run_tbss_preproc.sh
+++ Feb8_2018_run_tbss_preproc.sh
@@ -13,3 +13,145 @@ tbss_1_preproc *.nii.gz
 tbss_2_reg -T
 tbss_3_postreg -S
 tbss_4_prestats 0.2
+
+
+mkdir ${MAINDIR}/tbss/MD
+for f in 9001_SH-11644 9001_SH-11692 9001_SH-12271 9002_RA-11764 9002_RA-11833 9002_RA-12388 9004_EP-12203 9004_EP-12126 9004_EP-12955 9006_EO-12389 9006_EO-12487 9006_EO-13017
+do
+cp ${ANALYSISDIR}/${f}/diffusion/dtifit_MD.nii.gz ${MAINDIR}/tbss/MD/${f}_FA.nii.gz
+done
+tbss_non_FA MD
+
+mkdir ${MAINDIR}/tbss/L1
+for f in 9001_SH-11644 9001_SH-11692 9001_SH-12271 9002_RA-11764 9002_RA-11833 9002_RA-12388 9004_EP-12203 9004_EP-12126 9004_EP-12955 9006_EO-12389 9006_EO-12487 9006_EO-13017
+do
+cp ${ANALYSISDIR}/${f}/diffusion/dtifit_L1.nii.gz ${MAINDIR}/tbss/L1/${f}_FA.nii.gz
+done
+tbss_non_FA L1
+
+mkdir ${MAINDIR}/tbss/RD
+for f in 9001_SH-11644 9001_SH-11692 9001_SH-12271 9002_RA-11764 9002_RA-11833 9002_RA-12388 9004_EP-12203 9004_EP-12126 9004_EP-12955 9006_EO-12389 9006_EO-12487 9006_EO-13017
+do
+fslmaths ${ANALYSISDIR}/${f}/diffusion/dtifit_L2 -add ${ANALYSISDIR}/${f}/diffusion/dtifit_L3 -div 2 ${ANALYSISDIR}/${f}/diffusion/RD
+cp ${ANALYSISDIR}/${f}/diffusion/RD.nii.gz ${MAINDIR}/tbss/RD/${f}_FA.nii.gz
+done
+tbss_non_FA RD
+
+
+#split TBSS images so that the correct ones are associated with the correct subjects/timepoints
+
+fslsplit stats/all_FA_skeletonised stats/FA_skeleton -t
+
+mkdir ${ANALYSISDIR}/9001_SH_diffusion_longitudinal/tbss_images
+
+fslmaths stats/FA_skeleton0000 ${ANALYSISDIR}/9001_SH_diffusion_longitudinal/tbss_images/FA_pre
+
+fslmaths stats/FA_skeleton0001 ${ANALYSISDIR}/9001_SH_diffusion_longitudinal/tbss_images/FA_day1
+
+fslmaths stats/FA_skeleton0002 ${ANALYSISDIR}/9001_SH_diffusion_longitudinal/tbss_images/FA_3M
+
+mkdir ${ANALYSISDIR}/9002_RA_diffusion_longitudinal/tbss_images
+
+fslmaths stats/FA_skeleton0003 ${ANALYSISDIR}/9002_RA_diffusion_longitudinal/tbss_images/FA_pre
+
+fslmaths stats/FA_skeleton0004 ${ANALYSISDIR}/9002_RA_diffusion_longitudinal/tbss_images/FA_day1
+
+fslmaths stats/FA_skeleton0005 ${ANALYSISDIR}/9002_RA_diffusion_longitudinal/tbss_images/FA_3M
+
+mkdir ${ANALYSISDIR}/9004_EP_diffusion_longitudinal/tbss_images
+
+fslmaths stats/FA_skeleton0006 ${ANALYSISDIR}/9004_EP_diffusion_longitudinal/tbss_images/FA_pre
+
+fslmaths stats/FA_skeleton0007 ${ANALYSISDIR}/9004_EP_diffusion_longitudinal/tbss_images/FA_day1
+
+fslmaths stats/FA_skeleton0008 ${ANALYSISDIR}/9004_EP_diffusion_longitudinal/tbss_images/FA_3M
+
+mkdir ${ANALYSISDIR}/9006_EO_diffusion_longitudinal/tbss_images
+
+fslmaths stats/FA_skeleton0009 ${ANALYSISDIR}/9006_EO_diffusion_longitudinal/tbss_images/FA_pre
+
+fslmaths stats/FA_skeleton0010 ${ANALYSISDIR}/9006_EO_diffusion_longitudinal/tbss_images/FA_day1
+
+fslmaths stats/FA_skeleton0011 ${ANALYSISDIR}/9006_EO_diffusion_longitudinal/tbss_images/FA_3M
+
+fslsplit stats/all_MD_skeletonised stats/MD_skeleton -t
+
+fslmaths stats/MD_skeleton0000 ${ANALYSISDIR}/9001_SH_diffusion_longitudinal/tbss_images/MD_pre
+
+fslmaths stats/MD_skeleton0001 ${ANALYSISDIR}/9001_SH_diffusion_longitudinal/tbss_images/MD_day1
+
+fslmaths stats/MD_skeleton0002 ${ANALYSISDIR}/9001_SH_diffusion_longitudinal/tbss_images/MD_3M
+
+fslmaths stats/MD_skeleton0003 ${ANALYSISDIR}/9002_RA_diffusion_longitudinal/tbss_images/MD_pre
+
+fslmaths stats/MD_skeleton0004 ${ANALYSISDIR}/9002_RA_diffusion_longitudinal/tbss_images/MD_day1
+
+fslmaths stats/MD_skeleton0005 ${ANALYSISDIR}/9002_RA_diffusion_longitudinal/tbss_images/MD_3M
+
+fslmaths stats/MD_skeleton0006 ${ANALYSISDIR}/9004_EP_diffusion_longitudinal/tbss_images/MD_pre
+
+fslmaths stats/MD_skeleton0007 ${ANALYSISDIR}/9004_EP_diffusion_longitudinal/tbss_images/MD_day1
+
+fslmaths stats/MD_skeleton0008 ${ANALYSISDIR}/9004_EP_diffusion_longitudinal/tbss_images/MD_3M
+
+fslmaths stats/MD_skeleton0009 ${ANALYSISDIR}/9006_EO_diffusion_longitudinal/tbss_images/MD_pre
+
+fslmaths stats/MD_skeleton0010 ${ANALYSISDIR}/9006_EO_diffusion_longitudinal/tbss_images/MD_day1
+
+fslmaths stats/MD_skeleton0011 ${ANALYSISDIR}/9006_EO_diffusion_longitudinal/tbss_images/MD_3M
+
+
+fslsplit stats/all_L1_skeletonised stats/L1_skeleton -t
+
+fslmaths stats/L1_skeleton0000 ${ANALYSISDIR}/9001_SH_diffusion_longitudinal/tbss_images/L1_pre
+
+fslmaths stats/L1_skeleton0001 ${ANALYSISDIR}/9001_SH_diffusion_longitudinal/tbss_images/L1_day1
+
+fslmaths stats/L1_skeleton0002 ${ANALYSISDIR}/9001_SH_diffusion_longitudinal/tbss_images/L1_3M
+
+fslmaths stats/L1_skeleton0003 ${ANALYSISDIR}/9002_RA_diffusion_longitudinal/tbss_images/L1_pre
+
+fslmaths stats/L1_skeleton0004 ${ANALYSISDIR}/9002_RA_diffusion_longitudinal/tbss_images/L1_day1
+
+fslmaths stats/L1_skeleton0005 ${ANALYSISDIR}/9002_RA_diffusion_longitudinal/tbss_images/L1_3M
+
+fslmaths stats/L1_skeleton0006 ${ANALYSISDIR}/9004_EP_diffusion_longitudinal/tbss_images/L1_pre
+
+fslmaths stats/L1_skeleton0007 ${ANALYSISDIR}/9004_EP_diffusion_longitudinal/tbss_images/L1_day1
+
+fslmaths stats/L1_skeleton0008 ${ANALYSISDIR}/9004_EP_diffusion_longitudinal/tbss_images/L1_3M
+
+fslmaths stats/L1_skeleton0009 ${ANALYSISDIR}/9006_EO_diffusion_longitudinal/tbss_images/L1_pre
+
+fslmaths stats/L1_skeleton0010 ${ANALYSISDIR}/9006_EO_diffusion_longitudinal/tbss_images/L1_day1
+
+fslmaths stats/L1_skeleton0011 ${ANALYSISDIR}/9006_EO_diffusion_longitudinal/tbss_images/L1_3M
+
+
+fslsplit stats/all_RD_skeletonised stats/RD_skeleton -t
+
+fslmaths stats/RD_skeleton0000 ${ANALYSISDIR}/9001_SH_diffusion_longitudinal/tbss_images/RD_pre
+
+fslmaths stats/RD_skeleton0001 ${ANALYSISDIR}/9001_SH_diffusion_longitudinal/tbss_images/RD_day1
+
+fslmaths stats/RD_skeleton0002 ${ANALYSISDIR}/9001_SH_diffusion_longitudinal/tbss_images/RD_3M
+
+fslmaths stats/RD_skeleton0003 ${ANALYSISDIR}/9002_RA_diffusion_longitudinal/tbss_images/RD_pre
+
+fslmaths stats/RD_skeleton0004 ${ANALYSISDIR}/9002_RA_diffusion_longitudinal/tbss_images/RD_day1
+
+fslmaths stats/RD_skeleton0005 ${ANALYSISDIR}/9002_RA_diffusion_longitudinal/tbss_images/RD_3M
+
+fslmaths stats/RD_skeleton0006 ${ANALYSISDIR}/9004_EP_diffusion_longitudinal/tbss_images/RD_pre
+
+fslmaths stats/RD_skeleton0007 ${ANALYSISDIR}/9004_EP_diffusion_longitudinal/tbss_images/RD_day1
+
+fslmaths stats/RD_skeleton0008 ${ANALYSISDIR}/9004_EP_diffusion_longitudinal/tbss_images/RD_3M
+
+fslmaths stats/RD_skeleton0009 ${ANALYSISDIR}/9006_EO_diffusion_longitudinal/tbss_images/RD_pre
+
+fslmaths stats/RD_skeleton0010 ${ANALYSISDIR}/9006_EO_diffusion_longitudinal/tbss_images/RD_day1
+
+fslmaths stats/RD_skeleton0011 ${ANALYSISDIR}/9006_EO_diffusion_longitudinal/tbss_images/RD_3M
+
+
