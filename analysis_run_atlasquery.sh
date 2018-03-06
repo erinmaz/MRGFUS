ab8ecd1c2b591148c1655237c749f9beeaf95acd
Erin Mazerolle <erinmaz@gmail.com>
2018-03-06 10:58:19 -0700

    fixed typo

--- analysis_run_atlasquery.sh
+++ analysis_run_atlasquery.sh
@@ -5,5 +5,5 @@ for sub in `ls -d $lesiondir/9*`
 do
 subnum=`basename $sub`
 atlasquery -a "Histological Thalamus Atlas" -m ${sub}/T1_lesion_mask_filled2MNI_1mm > ${outdir}/${subnum}/HistThal_atlasquery_output.txt
-sed -i 's/^/'$subnum':/g' ${outdir}/${subnum}/HistThal_atlasquery_output.txt
+sed -i ' ' 's/^/'$subnum':/g' ${outdir}/${subnum}/HistThal_atlasquery_output.txt
 done
