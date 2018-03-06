c8c96e0dd3fbeb115dbcd2f832977547f5b4164b
Erin Mazerolle <erinmaz@gmail.com>
2018-03-06 11:03:48 -0700

    fixed sed call in analysis_run_atlasquery.sh

--- analysis_run_atlasquery.sh
+++ analysis_run_atlasquery.sh
@@ -5,5 +5,6 @@ for sub in `ls -d $lesiondir/9*`
 do
 subnum=`basename $sub`
 atlasquery -a "Histological Thalamus Atlas" -m ${sub}/T1_lesion_mask_filled2MNI_1mm > ${outdir}/${subnum}/HistThal_atlasquery_output.txt
-sed -i ' ' 's/^/'$subnum':/g' ${outdir}/${subnum}/HistThal_atlasquery_output.txt
+sed -i.bk 's/^/'$subnum':/g' ${outdir}/${subnum}/HistThal_atlasquery_output.txt
+rm ${outdir}/${subnum}/HistThal_atlasquery_output.txt.bk
 done
