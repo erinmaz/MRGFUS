61c7bd34bdcd65104de38ac78ffa940ec421ba77
Erin Mazerolle <erinmaz@gmail.com>
2018-03-06 12:08:11 -0700

    edited analysis_run_atlasquery.sh to create text file with all data

--- analysis_run_atlasquery.sh
+++ analysis_run_atlasquery.sh
@@ -8,3 +8,12 @@ atlasquery -a "Histological Thalamus Atlas" -m ${sub}/T1_lesion_mask_filled2MNI_
 sed -i.bk 's/^/'$subnum':/g' ${outdir}/${subnum}/HistThal_atlasquery_output.txt
 rm ${outdir}/${subnum}/HistThal_atlasquery_output.txt.bk
 done
+
+mystring=""
+for sub in `ls -d $lesiondir/9*`
+do
+subnum=`basename $sub`
+mystring=`echo $mystring ${outdir}/${subnum}/HistThal_atlasquery_output.txt`
+done
+cat $mystring > ${outdir}/HistThal_atlasquery_all.txt
+
