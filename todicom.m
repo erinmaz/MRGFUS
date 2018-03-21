4d076769df7d611e993b34c028c11f6f716e5e19
Erin Mazerolle <erinmaz@gmail.com>
2018-03-21 13:36:04 -0600

    fix todicom.m to update series uid

--- todicom.m
+++ todicom.m
@@ -1,15 +1,19 @@
+cd /Users/erin/Desktop/Projects/MRGFUS/analysis/9013_JD-13455/anat
 new = niftiread('T2_avg_Vim_burned_high_swap.nii');
 newflip=flipud(new);
 newflip2=fliplr(newflip);
 new_int=int16(newflip2);
-%cd into directory with original image
-files=dir('IM*');
+
+cd '/Users/erin/Desktop/Projects/MRGFUS/analysis/9013_JD-13455/anat/Orig-HorosOutput/9013_Jd/Mrgfus_PreTreatment(Zk_Fmc_Mrgfus_13455) - 13455/PUSag_CUBE_T2_400'
+uid=dicomuid;
+files=dir('IM*.dcm');
 for i=1:length(files)
 old_info=dicominfo(files(i).name);
 old_info.Filename=[pwd '/new/' sprintf('new%03d.dcm',i)];
 old_info.SeriesDescription='T2_avg_Vim_burned_high';
 old_info.SeriesNumber=9999;
-dicomwrite(new_int(:,:,i), sprintf('new%03d.dcm',i), old_info);
+old_info.SeriesInstanceUID=uid;
+dicomwrite(new_int(:,:,i), sprintf('new%03d.dcm',i), old_info, 'WritePrivate',true);
 end
 mkdir new
 system('mv new*.dcm new/.')
