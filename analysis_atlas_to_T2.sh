704858e2dd44e9b5483fe1cae5e774a6c52c319e
Erin Mazerolle <erinmaz@gmail.com>
2018-04-09 14:38:11 -0600

    code to process version of atlas from Ayca

--- analysis_atlas_to_T2.sh
+++ analysis_atlas_to_T2.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
-MYSUB=$1
+MYSUB=$1 #pretatment
 MAINDIR=/Users/erin/Desktop/Projects/MRGFUS
 ANATDIR=${MAINDIR}/analysis/${MYSUB}/anat
 XFMSDIR=${MAINDIR}/analysis/${MYSUB}/xfms
 mkdir $XFMSDIR
 
-bet ${ANATDIR}/T2 ${ANATDIR}/T2_brain -m -f .25 -S
+if [ -e ${MAINDIR}/analysis/${MYSUB}/anat/T2_2.nii.gz ]; then
 
-bet ${ANATDIR}/T2_2 ${ANATDIR}/T2_2_brain -m -f .25 -S
+	bet ${ANATDIR}/T2 ${ANATDIR}/T2_brain -m -f .25 -S
+	
+	bet ${ANATDIR}/T2_2 ${ANATDIR}/T2_2_brain -m -f .25 -S
 
-flirt -in ${ANATDIR}/T2_2 -ref ${ANATDIR}/T2 -out ${ANATDIR}/T2_2_to_T2 -dof 6 -omat ${XFMSDIR}/T2_2_to_T2.mat -nosearch
+	flirt -in ${ANATDIR}/T2_2 -ref ${ANATDIR}/T2 -out ${ANATDIR}/T2_2_to_T2 -dof 6 -omat ${XFMSDIR}/T2_2_to_T2.mat -nosearch
 
 #flirt -in ${ANATDIR}/T2_2_brain -ref ${ANATDIR}/T2_brain -out ${ANATDIR}/T2_2_brain_to_T2_brain_ns -dof 6 -omat ${XFMSDIR}/T2_2_brain_to_T2_brain_ns.mat -nosearch
 
-fsleyes ${ANATDIR}/T2_2_to_T2 ${ANATDIR}/T2
+	fsleyes ${ANATDIR}/T2_2_to_T2 ${ANATDIR}/T2
 
 #the results of this command were basically the same as the non brain extracted versoin
 #fsleyes ${ANATDIR}/T2_2_brain_to_T2_brain_ns  ${ANATDIR}/T2_brain
 
-fslmaths ${ANATDIR}/T2_2_to_T2 -add ${ANATDIR}/T2 -div 2 ${ANATDIR}/T2_avg
+	fslmaths ${ANATDIR}/T2_2_to_T2 -add ${ANATDIR}/T2 -div 2 ${ANATDIR}/T2_avg
 
-bet ${ANATDIR}/T2_avg ${ANATDIR}/T2_avg_brain -m -f 0.25 -S
+	bet ${ANATDIR}/T2_avg ${ANATDIR}/T2_avg_brain -m -f 0.25 -S
+
+else
+
+	fslmaths ${ANATDIR}/T2 ${ANATDIR}/T2_avg
+
+fi
 
 fslmaths ${ANATDIR}/T2_avg ${XFMSDIR}/T2_avg
 
@@ -46,7 +54,7 @@ fsleyes ${ANATDIR}/T2_avg ${ANATDIR}/Vim_to_T2_avg
 
 fslmaths ${ANATDIR}/Vim_to_T2_avg -binv ${ANATDIR}/Vim_to_T2_avg_mask
 
-fslmaths ${ANATDIR}/T2_avg -mas ${ANATDIR}/Vim_to_T2_avg_mask ${ANATDIR}/T2_avg_Vim_burned_low
+#fslmaths ${ANATDIR}/T2_avg -mas ${ANATDIR}/Vim_to_T2_avg_mask ${ANATDIR}/T2_avg_Vim_burned_low
 
 rangearray=(`fslstats ${ANATDIR}/T2_avg -R`)
 
@@ -54,10 +62,11 @@ fslmaths ${ANATDIR}/Vim_to_T2_avg -mul 2 -mul ${rangearray[1]} ${ANATDIR}/Vim_to
 
 fslmaths ${ANATDIR}/T2_avg -add ${ANATDIR}/Vim_to_T2_avg_mul ${ANATDIR}/T2_avg_Vim_burned_high
 
-fsleyes ${ANATDIR}/T2_avg_Vim_burned_low ${ANATDIR}/T2_avg_Vim_burned_high
+#fsleyes ${ANATDIR}/T2_avg_Vim_burned_low ${ANATDIR}/T2_avg_Vim_burned_high
+fsleyes ${ANATDIR}/T2_avg_Vim_burned_high
 
 fslchfiletype NIFTI ${ANATDIR}/T2_avg_Vim_burned_high
-fslchfiletype NIFTI ${ANATDIR}/T2_avg_Vim_burned_low 
+#fslchfiletype NIFTI ${ANATDIR}/T2_avg_Vim_burned_low 
 
 #did this swapdim although I still end up flipping things in matlab. Final output dicom does not appear to be l-r flipped
 fslswapdim  ${ANATDIR}/T2_avg_Vim_burned_high.nii z y -x  ${ANATDIR}/T2_avg_Vim_burned_high_swap
@@ -66,17 +75,15 @@ fslchfiletype NIFTI ${ANATDIR}/T2_avg_Vim_burned_high_swap
 
 #other ideas for nii2dcm:
 #Then use plugin in Horos to convert from nii 2 dcm
-https://www.researchgate.net/post/How_can_one_convert_Nifti_to_DICOM_for_DTI_images
-A plugin in Osirix does the trick: plugin>DataBase>Nifti to Dicom
-If you don't have it, you can download the plugin from there: 
-http://deqiang.webege.com/software/OsirixPlugins/NiftiToDICOM.html
-3 years ago
+#https://www.researchgate.net/post/How_can_one_convert_Nifti_to_DICOM_for_DTI_images
+#A plugin in Osirix does the trick: plugin>DataBase>Nifti to Dicom
+#If you don't have it, you can download the plugin from there: 
+#http://deqiang.webege.com/software/OsirixPlugins/NiftiToDICOM.html
+#3 years ago
 
-IF I HAVE PROBLEMS, could also try this:
-Rajat M. Thomas
-Netherlands Institute for Neuroscience
-http://xmedcon.sourceforge.net/Main/Download
-Worked quite easily for NIFTI to DICOM conversion
-1 Recommendation
-
-
+#IF I HAVE PROBLEMS, could also try this:
+#Rajat M. Thomas
+#Netherlands Institute for Neuroscience
+#http://xmedcon.sourceforge.net/Main/Download
+#Worked quite easily for NIFTI to DICOM conversion
+#1 Recommendation
