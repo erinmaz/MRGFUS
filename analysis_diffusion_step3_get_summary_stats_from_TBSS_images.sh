264471b7c21c1b1bc652f3625824d81f50c61f7e
Erin Mazerolle <erinmaz@gmail.com>
2018-05-17 14:26:05 -0600

    analysis_diffusion_step3 - data used in MD neuroteam retreat

--- analysis_diffusion_step3_get_summary_stats_from_TBSS_images.sh
+++ analysis_diffusion_step3_get_summary_stats_from_TBSS_images.sh
@@ -3,11 +3,11 @@
 # Check especially how inferior tracts look relative to skeleton, on contralateral and ipsilateral side (relative to lesion)
 
 # PROBTRACKX OUTPUT PREFIX FROM analysis_diffusion_step2_track_pre_from_day1_lesion.sh
-TRACT_OUTPUT=150518
+TRACT_OUTPUT=tracking_day1_lesion_150518
 
 #OUTPUT FROM analysis_diffusion_step1B_run_tbss_preproc.sh
-TBSSDIR=/Users/erin/Desktop/Projects/MRGFUS/tbss_140518
-
+TBSSNAME=tbss_140518
+TBSSDIR=/Users/erin/Desktop/Projects/MRGFUS/${TBSSNAME}
 ANALYSISDIR=/Users/erin/Desktop/Projects/MRGFUS/analysis 
 i=0
 
@@ -15,27 +15,23 @@ i=0
 subs=(9001_SH 9002_RA 9004_EP 9005_BG 9006_EO 9007_RB 9009_CRB)
 for MYSUB_TOTRACK in 9001_SH-11644 9002_RA-11764 9004_EP-12126 9005_BG-13004 9006_EO-12389 9007_RB-12461 9009_CRB-12609 
 do 
-  for theseed in ${TRACT_OUTPUT} ${TRACT_OUTPUT}_contralateral
-  do
-    for roi in ${ANALYSISDIR}/${MYSUB_TOTRACK}/diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin ${ANALYSISDIR}/${MYSUB_TOTRACK}/diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion ${ANALYSISDIR}/${MYSUB_TOTRACK}/diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_thalamus 
-${ANALYSISDIR}/${MYSUB_TOTRACK}/diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_orneighbours_thalamus
-${ANALYSISDIR}/${MYSUB_TOTRACK}/diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_nothalamus ${ANALYSISDIR}/${MYSUB_TOTRACK}/diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_superior ${ANALYSISDIR}/${MYSUB_TOTRACK}/diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_inferior ${ANALYSISDIR}/${MYSUB_TOTRACK}/diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_superior_thalamus ${ANALYSISDIR}/${MYSUB_TOTRACK}/diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_inferior_thalamus ${ANALYSISDIR}/${MYSUB_TOTRACK}/diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_superior_nothalamus ${ANALYSISDIR}/${MYSUB_TOTRACK}/diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_inferior_nothalamus ${ANALYSISDIR}/${MYSUB_TOTRACK}/diffusion/rois_${TRACT_OUTPUT}/${theseed}_neighbouringvoxels 
-${ANALYSISDIR}/${MYSUB_TOTRACK}/diffusion/rois_${TRACT_OUTPUT}/${theseed}
-    do
-      applywarp -i ${roi} -o ${roi}2standard_tbss_warp -r $FSLDIR/data/standard/FMRIB58_FA_1mm -w ${TBSSDIR}/FA/${MYSUB_TOTRACK}_FA_FA_to_target_warp --interp=spline
-      fslmaths ${roi}2standard_tbss_warp -thr 0.5 -bin ${roi}2standard_tbss_warp
-      for time in pre day1 3M
-      do
-		for measure in FA MD RD L1
+	for theseed in ${TRACT_OUTPUT} ${TRACT_OUTPUT}_contralateral
+	do
+		for roi in diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_thalamus diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_orneighbours_thalamus diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_nothalamus diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_superior diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_inferior diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_superior_thalamus diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_inferior_thalamus diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_superior_nothalamus diffusion.bedpostX/${theseed}/fdt_paths_norm_thr0.01_bin_nolesion_inferior_nothalamus diffusion/rois_${TRACT_OUTPUT}/${theseed}_neighbouringvoxels diffusion/rois_${TRACT_OUTPUT}/${theseed}
 		do
-		  for type in images skeleton
-          do
-			echo ${subs[$i]} $roi $time $measure `fslstats ${subs[$i]}_diffusion_longitudinal/tbss_${TRACT_OUTPUT}_${type}/${measure}_${time} -k ${roi}2standard_tbss_warp -M -V` >> tractography_TBSS_${TRACT_OUTPUT}_${type}_output_mean_vol.txt
-		  done
-        done
-	  done
+			applywarp -i ${ANALYSISDIR}/${MYSUB_TOTRACK}/${roi} -o ${ANALYSISDIR}/${MYSUB_TOTRACK}/${roi}2standard_tbss_warp -r $FSLDIR/data/standard/FMRIB58_FA_1mm -w ${TBSSDIR}/FA/${MYSUB_TOTRACK}_FA_FA_to_target_warp --interp=spline
+			fslmaths ${ANALYSISDIR}/${MYSUB_TOTRACK}/${roi}2standard_tbss_warp -thr 0.5 -bin ${ANALYSISDIR}/${MYSUB_TOTRACK}/${roi}2standard_tbss_warp
+			for scan in pre day1 3M
+			do
+				for measure in FA MD RD L1
+				do
+					for type in image skeleton
+					do
+						echo ${subs[$i]} $roi $scan $measure $type `fslstats ${ANALYSISDIR}/${subs[$i]}_diffusion_longitudinal/${TBSSNAME}_${type}/${measure}_${scan} -k ${ANALYSISDIR}/${MYSUB_TOTRACK}/${roi}2standard_tbss_warp -M -V` >> ${ANALYSISDIR}/tractography_${TBSSNAME}_${TRACT_OUTPUT}_${type}_output_mean_vol_regen.txt
+					done
+				done
+			done
+		done
 	done
-  done
-  let i=$i+1
+	let i=$i+1
 done
-
---
+++ figs.m
@@ -0,0 +1,34 @@
+%LESION VOLUME FIGURE, 051518
+%copied from google sheet MRGFUS/Lesion Volume - Working
+%https://docs.google.com/spreadsheets/d/1hkB76ZZJLdYTov6ueGIEtb-8jEMdD2Ieg6oPrHuvnoA/edit?usp=sharing
+data1=[1	230
+1	141
+1	416
+1	180
+1	442
+1	182
+1	202
+1	396];
+data2=[2	47
+2	20
+2	94
+2	13
+2	31
+2	7
+2	1
+2	63];
+
+colours = {'r','b','g','k','r','b','g','k'};
+markers = {'o','o','o','o','*','*','*','*'};
+figure
+hold
+for i=1:length(data1)
+scatter(data1(i,1),data1(i,2),colours{i},markers{i});
+scatter(data2(i,1),data2(i,2),colours{i},markers{i});
+end
+set(gca,'fontsize', 18);
+axis([0 3 0 500]);
+xticks([1 2]);
+xticklabels({'Day 1','Day 90'});
+xlabel('Time since treatment');
+ylabel('T1w lesion volume (mm^3)');
