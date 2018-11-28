1f3460fab16e4ce971f0fe0debc5a1bb32d6d245
Erin Mazerolle <erinmaz@gmail.com>
2018-11-28 14:09:32 -0700

    fix typos

--- analysis_avg_T2s_day1.sh
+++ analysis_avg_T2s_day1.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 #did 9010 manually
 cd /Users/erin/Desktop/Projects/MRGFUS/analysis
-for sub in 9011_BB-14878 9013_JD-14227 9020_JL-14836 9021_WM-15089
+for sub in 9011_BB-14148 9013_JD-13722 9016_EB-14450 9020_JL-14340 9021_WM-14455
 do
 flirt -in ${sub}/anat/T2_2 -ref ${sub}/anat/T2 -out ${sub}/anat/T2_2_to_T2 -omat ${sub}/anat/T2_2_to_T2.mat -dof 6
-fslmaths ${sub}/anat/T2_2_to_T2 -add${sub}/anat/ T2 -div 2 ${sub}/anat/T2_avg
+fslmaths ${sub}/anat/T2_2_to_T2 -add ${sub}/anat/ T2 -div 2 ${sub}/anat/T2_avg
 done
