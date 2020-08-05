533ee702f26957b2e03ada04d6fb2dbae91a5bea
Erin Mazerolle <erinmaz@gmail.com>
2020-08-05 10:48:13 -0300

    manuscript 2020 08 05

--- analysis_Kwon_ROIs_probtrackx_thalterm_check_lesion_overlap_ants.sh
+++ analysis_Kwon_ROIs_probtrackx_thalterm_check_lesion_overlap_ants.sh
@@ -3,7 +3,7 @@ ANALYSISDIR=${MAINDIR}/analysis
 
 
 LESIONFILE=anat/xfms/ants/bet/T1_lesion_filled_mask_2_MNI152_T1_1mm.nii.gz
-for MYSUB in 9001_SH 9002_RA 9004_EP 9005_BG 9006_EO 9007_RB 9009_CRB 9011_BB 9013_JD 9021_WM 9022_JG 9023_WS
+for MYSUB in 9001_SH 9004_EP 9005_BG 9006_EO 9007_RB 9009_CRB 9011_BB 9013_JD 9021_WM 9022_JG 9023_WS 9024_LLB 9028_PR 9030_GA 9031_DB
 do
 
 PRE_EXAM=`sed -n '/'${MYSUB}'/p' ~/Desktop/Projects/MRGFUS/scripts/IDs_and_ExamNums.sh | awk '{print $2}'` 
--- run_20200804.sh
+++ run_20200804.sh
@@ -10,3 +10,8 @@ fslstats results_20200804_heatmap_day1 -R # to get range for figure
 
 
 #manually create a new spreadsheet (Results_Summary_manuscript_20200804.xls), update 9001, remove 9002, and add 9024, 9028, 9030, and 9031
+
+#for figure
+flirt -in /Users/erin/Desktop/Projects/MRGFUS/analysis/9001_SH-11692/anat/T1.nii.gz -out /Users/erin/Desktop/Projects/MRGFUS/analysis/9001_SH_longitudinal_xfms_T1/T1_day1_2_pre_6dof -applyxfm -init /Users/erin/Desktop/Projects/MRGFUS/analysis/9001_SH_longitudinal_xfms_T1/T1_brain_day1_2_pre_6dof.mat -ref /Users/erin/Desktop/Projects/MRGFUS/analysis/9001_SH-11644/anat/T1.nii.gz  
+
+analysis_Kwon_ROIs_probtrackx_thalterm_check_lesion_overlap_ants.sh
