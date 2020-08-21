8e22f96036f55e62589c7f65cfa0156988dbe6ee
Erin Mazerolle <erinmaz@gmail.com>
2020-08-21 10:24:37 -0300

    check ROI size of untreated tract ROIs

---
+++ run_20200821.sh
@@ -0,0 +1,7 @@
+for MYSUB in 9001_SH 9004_EP 9005_BG 9006_EO 9007_RB 9009_CRB 9010_RR 9011_BB 9013_JD 9016_EB 9021_WM 9022_JG 9023_WS 9024_LLB 9028_PR 9030_GA 9031_DB 
+do
+PRE=`sed -n '/'${MYSUB}'/p' ~/Desktop/Projects/MRGFUS/scripts/IDs_and_ExamNums.sh | awk '{print $2}'` 
+DAY1=`sed -n '/'${MYSUB}'/p' ~/Desktop/Projects/MRGFUS/scripts/IDs_and_ExamNums.sh | awk '{print $3}'` 
+#MONTH3=`sed -n '/'${MYSUB}'/p' ~/Desktop/Projects/MRGFUS/scripts/IDs_and_ExamNums.sh | awk '{print $4}'`
+analysis_Kwon_ROIs_probtrackx_get_untreated_tract_ants.sh $MYSUB $PRE $DAY1
+done
